eb2605a539c96f5db986fb36e8456baab9432770
sam <samuel_ach67@yahoo.com>
2020-05-05 23:05:06 -0400

    1. This is assignment 1

---
+++ Assignments/src/Assignment1.java
@@ -0,0 +1,27 @@
+
+
+public class Assignment1 {
+
+    /**
+     * Assignment Instructions
+     * 1. create two integer variables in main method age1 and age2
+     * 2. Create a method ageGroup that takes the two ages from 1. above and do the following
+     *  i. Print "one of you is old" if either age1 or age2 is greater than 50
+     *  ii. Print "You are both old" if both age1 and age2 are greater than 50
+     *  iii. Print "one of you is young" if either age1 or age2 is less than 40
+     *  iv. Print "you are both young"  if both age1 and age2 are less than 40
+     *
+     *
+     *
+     *  3. Create an array with the following values "1,4,6,7,56,34,23"
+     *  4. Create a method greaterThan20 which prints all numbers in the array that are greater than 20.
+     *
+     * HAPPY CODING!!
+     *
+     * GOOD LUCK!!
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
