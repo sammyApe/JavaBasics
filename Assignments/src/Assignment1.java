b12e3c58679a61ca6fe166a1744472cac8fdf18d
Sam <sacheampong@merchante-solutions.com>
2020-05-21 19:27:22 -0400

    I added new questions

--- Assignments/src/Assignment1.java
+++ Assignments/src/Assignment1.java
@@ -16,12 +16,36 @@ public class Assignment1 {
      *  3. Create an array with the following values "1,4,6,7,56,34,23"
      *  4. Create a method greaterThan20 which prints all numbers in the array that are greater than 20.
      *
+     *  5. Let ageGroup2(age1,age2) return a string
+     *  6. get the value returned from ageGroup and add your name to it;
+     *
+     *  7. Create a method greaterThan20 which returns all numbers in the array that are greater than 20.
+     *
      * HAPPY CODING!!
      *
      * GOOD LUCK!!
      */
 
     public static void main(String[] args) {
-        System.out.println("I am inside main ");
+        //1. create two integer variables in main method age1 and age2
+        int age1 = 23 ;
+        int age2 = 30;
+
+        ageGroup(age1,age2);
+
     }
+
+
+
+    private static void ageGroup(int age1, int age2) {
+
+        if(age1 > 50 || age2 > 50) System.out.println("one of you is old");
+        if(age1 > 50 && age2 > 50) System.out.println("You are both old");
+        if(age1 < 40 || age2 < 40) System.out.println("one of you is young");
+
+    }
+
+
+
+
 }
